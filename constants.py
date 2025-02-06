6f2a484cc78b9d17afdc1ca921e2e76cc43f5e8a
lili7 <lili7achatova@gmail.com>
2025-02-06 12:41:18 +0300

    Lesson1. Selenium ui Fixes after review

---
+++ constants.py
@@ -0,0 +1,7 @@
+from pycparser.c_ast import Constant
+from selenium.webdriver.common.devtools.v130.fed_cm import LoginState
+
+ADRESS = "https://jdi-testing.github.io/jdi-light/index.html"
+LOGIN = "Roman"
+PASSWORD = "Jdi1234"
+HAME = "ROMAN IOVLEV"
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import service, Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+import constants
+
 driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
 
-driver.get("https://jdi-testing.github.io/jdi-light/index.html")
+driver.get(constants.ADRESS)
 
 elem = driver.find_element(selenium.webdriver.common.by.By.CLASS_NAME, "profile-photo")
 
@@ -17,12 +19,12 @@ elem.click()
 
 elem1 = driver.find_element(selenium.webdriver.common.by.By.ID, "name")
 
-elem1.send_keys("Roman")
+elem1.send_keys(constants.LOGIN)
 
 
 elem2 = driver.find_element(selenium.webdriver.common.by.By.ID, "password")
 
-elem2.send_keys("Jdi1234")
+elem2.send_keys(constants.PASSWORD)
 
 
 elem3 = driver.find_element(selenium.webdriver.common.by.By.ID, "login-button")
@@ -33,4 +35,4 @@ elem3.click()
 elem4 = driver.find_element(selenium.webdriver.common.by.By.CLASS_NAME, "logout")
 elem5 = driver.find_element(selenium.webdriver.common.by.By.ID, "user-name")
 assert elem4.is_displayed()
-assert elem5.text == "ROMAN IOVLEV"
+assert elem5.text == constants.HAME
